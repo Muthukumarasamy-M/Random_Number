d8a6074115ba8766c0f7a546c58dbeefe2ac2b33
Muthukumarasamy-M <muthukumarasamy2019@gmail.com>
2024-01-12 14:34:09 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const num = document.getElementById("mylabel");
+const btn = document.getElementById("mybutton");
+const max = document.getElementById("max");
+const min = document.getElementById("min");
+btn.onclick = function () {
+  let m = Number(max.value);
+  let n = Number(min.value);
+  if (Boolean(m) && Boolean(n)) {
+    num.textContent = "";
+  } else {
+    num.textContent = Math.floor(Math.random() * (m - n + 1)) + n;
+  }
+};
